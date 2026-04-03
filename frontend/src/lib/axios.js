1f3588fc960c46ff6af69224de0b8b660393d180
biposhyan <chakma2107121@stud.kuet.ac.bd>
2026-04-04 01:22:03 +0600

    axios updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://bhalobasha.onrender.com',
+    baseURL: import.meta.env.VITE_API_URL || 'https://bhalo-basha-api.onrender.com',
     withCredentials: true
 })
 
